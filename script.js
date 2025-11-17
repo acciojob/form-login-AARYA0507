135b5313d4119e1f28487dd50c358638f5074a8b
AARYA0507 <138356395+AARYA0507@users.noreply.github.com>
2025-11-18 01:27:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 function getFormvalue() {
-    //Write your code here
-
+    
+    event.preventDefault();
+    const firstName = document.querySelector('[name="fname"]').value.trim();
+    const lastName = document.querySelector('[name="lname"]').value.trim();
+    const fullName = firstName + " " + lastName;
+    alert(fullName);
 }
